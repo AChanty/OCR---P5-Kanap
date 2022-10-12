403b639de5f853d75552e0893bdfd82d67d4ca58
Anthony Chanty <anthony.chanty@hotmail.com>
2022-10-12 23:56:34 +0200

    feat add color choice, add dynamic cart array

--- front/js/product.js
+++ front/js/product.js
@@ -6,18 +6,20 @@ fetch("http://localhost:3000/api/products") // appelle l'API
     })
     .then(function (product) {
 
+        const currenttUrl = window.location.href; // récupère l'URL de la page actuelle
+        const url = new URL(currenttUrl);
+        const idUrl = url.searchParams.get("id"); // récupère l'id dans l'URL actuel
 
-        for (let i = 0; i < product.length; i++) {
+        const myProduct = product.find((sofa) => { // fonction qui va vérifier dans la liste des produits si on a un id correspondant à l'id contenu dans l'url de la page
+            return sofa._id === idUrl
+        });
 
-            const { name, description, altTxt, imageUrl, _id: id, price, colors } = product[i]
+        if (myProduct) { // donne des instructions lorsque l'url correspond bien à l'id du produit, pour bien faire correspondre les deux
+            const { name, description, altTxt, imageUrl, _id: id, price, colors } = myProduct
 
-            const currenttUrl = window.location.href; // récupère l'URL de la page actuelle
-            const url = new URL(currenttUrl);
-            const idUrl = url.searchParams.get("id"); // récupère l'id dans l'URL actuel
-            console.log(idUrl);
+           
 
-            if (idUrl === id) { // donne des instructions lorsque l'url correspond bien à l'id du produit, pour bien faire correspondre les deux
-                console.log(id)
+            // if (idUrl === id) { // donne des instructions lorsque l'url correspond bien à l'id du produit, pour bien faire correspondre les deux
 
                 // créé un nouvel élément <img> dans la classe "item__img"'
                 let imgContainer = document.querySelector(".item__img")
@@ -38,21 +40,113 @@ fetch("http://localhost:3000/api/products") // appelle l'API
                 let itemPrice = document.getElementById('price')
                 itemPrice.textContent = price
 
-                // ajoute les options de couleurs
-                for (let i = 0; i < product[i].colors.length; i++) {
+
+                insertOptions(colors)
+                function insertOptions(colorList) { // insère les options de couleurs
                     let itemOptions = document.getElementById('colors')
-                    let newOption = document.createElement('option')
-                    newOption.setAttribute("value", product[i].colors[i])
-                    newOption.textContent = product[i].colors[i]
-                    itemOptions.appendChild(newOption)
+                    
+                    for (let i = 0; i < colorList.length; i++) { // pour chaque option de couleur disponible pour le produit, créé une balise HTML "option" avec la valeur de la couleur
+                        let newOption = document.createElement('option')
+                        itemOptions.appendChild(newOption)
+                        newOption.setAttribute("value", colorList[i])
+                        newOption.textContent = colorList[i]
+                    }
                 }
 
+                // }
 
-                break; // arrête la boucle lorsque l'id de l'url correspond à l'id du produit
 
-            }
+                    ////////////////////////////////////////////////////////////////
+
+            let cartArray = [
+            //     {
+            //     // "id": "",
+            //     // "color": "",
+            //     // "amount": 0
+            // }
+        ]
+
+            let addButton = document.getElementById('addToCart')
+            let quantityTotal = 0
+
+
+
+            addButton.addEventListener('click', function () {
+                let quantity = document.getElementById('quantity')
+                let quantityChosen = quantity.value
+                let colorsList = document.getElementById('colors')
+                let colorChosen = colorsList.options[colorsList.selectedIndex].value // cible la couleur choisie parmi les options disponibles dans la balise HTML "select".colors
+                // console.log(parseInt(quantityChosen))
+
+                function lookForId() {
+                    cartArray.map(object => {
+                        if (object.id === id) {
+                            return {...object, number: "55"}
+                        }
+                    })
+                }
+
+                function insertNewItem() {
+                    cartArray.push({"id": id, "color": colorChosen, "amount": Number(quantityChosen)})
+                }
+
+                if (colorChosen == false || quantityChosen == 0) { // si une couleur ou un nombre d'article n'est pas indiqué, renvoie une alerte d'erreur
+                    alert("Veuillez choisir une couleur et un nombre d'article à ajouter au panier")
+
+                // } else if (cartArray.includes(myProduct)) { // si le produit est déjà présent dans le panier
+                //     quantityTotal += Number(quantityChosen)
+                //     console.log(parseInt(quantityTotal))
+                //     console.log(quantityTotal)
+                //     console.log("// si le produit est déjà présent dans le panier")
+
+                // product.find((sofa) => { return sofa._id === idUrl })
+                
+                    // !cartArray.find((product) => { return product.id === myProduct })
+                
+                } else if (!cartArray.includes(myProduct) && !cartArray.includes(colorChosen) && !cartArray.find( product => product.id === id)) { // si le produit et la couleur ne sont pas déjà présents dans le panier
+                    quantityTotal += Number(quantityChosen)
+                    // cartArray.push(id, colorChosen, Number(quantityChosen))
+                    // cartArray.push({"id": id, "color": colorChosen, "amount": Number(quantityChosen)})
+                    insertNewItem() // = cartArray.push({"id": id, "color": colorChosen, "amount": Number(quantityChosen)})
+                    // newArray(id, colorChosen, quantityTotal)
+                    // cartArray.push(colorChosen)
+                    // cartArray.productId = id
+                    // cartArray.productColor = colorChosen
+                    // cartArray.productAmount = quantityChosen
+                    console.log("1. ni le produit ni la couleur ne sont pas déjà présents dans le panier")
+
+                } else if (cartArray.find( product => product.id === id) && !cartArray.find( product => product.color === colorChosen)) { // si le produit est déjà présent mais pas la variation de couleur, on ajoute un nouvel array
+                    // quantityTotal += Number(quantityChosen)
+                    insertNewItem() // = cartArray.push({"id": id, "color": colorChosen, "amount": Number(quantityChosen)})
+                    console.log("2. produit présent mais nouvelle couleur")
+                    // console.log("// si le produit est déjà présent dans le panier")
+    
+                // } else if (cartArray.find( product => product.id === id)){
+                } else if (cartArray.find( product => product.id === id) && cartArray.find( product => product.color === colorChosen)) { // si le produit et la couleur sont déjà présents dans le panier, on ajoute la quantité à la couleur choisie
+                    const newAmount = cartArray.findIndex ((obj => obj.color == colorChosen))
+                    cartArray[newAmount].amount += Number(quantityChosen)
+                    console.log("3. produit et couleur déjà présents, actualisation du montant")
+                }
+
+                function newArray(id, colorChosen, quantityTotal) {
+                    cartArray.push(new Array(id, colorChosen, quantityTotal))
+                }
+
+                let cartStorage = JSON.stringify(cartArray) // stocke les données du panier dans le localstorage
+                localStorage.setItem("cart", cartStorage)
+
+                localStorage.getItem("cart")
+
+                // let cartStorageLecture = localStorage.getItem("cart");
+                // let objJson = JSON.parse(cartStorageLecture);
+                // alert(cartStorage)
+
+                console.log(cartArray)
+            })
+
+                        // !! recalculer le total du panier dans la page panier, pas à chaque fois dans le local storage
 
         }
 
-    }
+        }
     )
--- front/js/script.js
+++ front/js/script.js
@@ -5,56 +5,10 @@ fetch("http://localhost:3000/api/products") // appelle l'API
         }
     })
     .then(function (product) {
-        // document
-        //     .querySelector("#items img")
-        //      met à jour les valeurs "src" et "alt"
-        //     .setAttribute("src", product[0].imageUrl)
-        //     .setAttribute("alt", product[0].altTxt);
-
-        // document
-        //     .querySelector("#items .productName")
-        //     .innerText = product[0].name;
-
-        // document
-        //     .querySelector("#items .productDescription")
-        //     .innerText = product[0].description;
-
-        // suppression de l'élément HTML contenant le canapé type, afin qu'ils soient tous générés via la boucle ci-dessous
-        // const supp = document.querySelector("#items a")
-        // supp.remove()
 
         // création d'une boucle qui va recréer le template HTML du canapé tant que la condition est valide (= longueur du tableau des produits), puis la boucle va
         // parcourir les infos du tableau pour les insérer dans les différentes valeurs [i]
-        // i = 1 si je laisse le premier canapé en HTML
         for (let i = 0; i < product.length; i++) {
-
-            // // créé un nouvel élément <a> dans la <section> #items
-            // let itemsSection = document.getElementById("items")
-            // let newLink = document.createElement('a');
-            // newLink.setAttribute("href", `./product.html?id=${product[i]._id}`)
-            // itemsSection.appendChild(newLink);
-
-            // // créé un nouvel élément <article> dans le nouvel élément <a>
-            // let newArticle = document.createElement('article');
-            // newLink.appendChild(newArticle);
-
-            // // créé un nouvel élément <img> dans le nouvel élément <article>
-            // let newImg = document.createElement('img');
-            // newImg.setAttribute("src", product[i].imageUrl)
-            // newImg.setAttribute("alt", product[i].altTxt);
-            // newArticle.appendChild(newImg);
-
-            // // créé un nouvel élément <h3> dans le nouvel élément <article>
-            // let newTitle = document.createElement('h3');
-            // newTitle.textContent = product[i].name;
-            // newArticle.appendChild(newTitle);
-
-            // // créé un nouvel élément <p> dans le nouvel élément <article>
-            // let newParagraph = document.createElement('p');
-            // newParagraph.textContent = product[i].description;
-            // newArticle.appendChild(newParagraph);
-
-            //////////////////////////////////////////////////////////////
             
             const { name, description, altTxt, imageUrl, _id: id } = product[i]
 
