b9fb43e0c9da3230f7e7177f7dadf54724af1fab
Anthony Chanty <anthony.chanty@hotmail.com>
2022-10-14 01:24:16 +0200

    feat add dynamic content to cart page

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,10 +1,6 @@
-const cart = []
-
-let cartStorageLecture = localStorage.getItem("cart");
-let cartStorage = JSON.parse(cartStorageLecture);
-// alert(cartStorageLecture)
-
-console.log(cartStorage.length)
+// permet la lecture du localstorage en reconvertissant le contenu de chaîne de caractères en objets
+let cartStorageLecture = localStorage.getItem("cart")
+let cartStorage = JSON.parse(cartStorageLecture)
 
 fetch("http://localhost:3000/api/products") // appelle l'API
     .then(function (response) {
@@ -14,119 +10,152 @@ fetch("http://localhost:3000/api/products") // appelle l'API
     })
     .then(function (product) {
 
-for (i = 0; i < cartStorage.length; i++) { // effectue une boucle pour chaque article différent dans le panier en localStorage
+        for (i = 0; i < cartStorage.length; i++) { // effectue une boucle pour chaque article différent dans le panier en localStorage
 
-    // mise en place de la structure HTML
+            const { id, color, amount } = cartStorage[i]
 
-    // créer un élément article.cart__item dans le parent section#cart__items
-    let sectionContainer = document.getElementById("cart__items")
-    let newArticle = document.createElement('article')
-    newArticle.classList.add("cart__item")
-    sectionContainer.appendChild(newArticle)
+            ////
+            // faire correspondre le produit du panier au produit dans l'API
+            const myProduct = product.find((sofa) => { // fonction qui va vérifier dans la liste des produits si on a un id correspondant à l'id contenu dans le localstorage
+                return sofa._id === id
+            })
 
-    // créer un élément div.cart__item__img dans le parent article.cart__item
-    let imgContainer = document.createElement('div')
-    imgContainer.classList.add("cart__item__img")
-    newArticle.appendChild(imgContainer)
+            if (myProduct) {
+                console.log("ok")
+                myProduct[i] = product
+            }
 
-    // créer un élément img dans le parent div.cart__item__img
-    // contient l'image du produit
-    let newImg = document.createElement('img')
-    imgContainer.appendChild(newImg)
-    newImg.setAttribute("src", "https://via.placeholder.com/350")
-    newImg.setAttribute("alt", "test")
+            const { name, altTxt, imageUrl, price } = myProduct
+            ////
 
-    // créer un élément div.cart__item__content dans le parent article.cart__item
-    let itemContent = document.createElement('div')
-    itemContent.classList.add("cart__item__content")
-    newArticle.appendChild(itemContent)
+            // mise en place de la structure HTML
 
-    // créer un élément div.cart__item__content__description dans le parent div.cart__item__content
-    let itemContentDescription = document.createElement('div')
-    itemContentDescription.classList.add("cart__item__content__description")
-    itemContent.appendChild(itemContentDescription)
+            // créer un élément article.cart__item dans le parent section#cart__items
+            let sectionContainer = document.getElementById("cart__items")
+            let newArticle = document.createElement('article')
+            newArticle.classList.add("cart__item")
+            sectionContainer.appendChild(newArticle)
 
-    // créer un élément h2 dans le parent div.cart__item__content__description
-    // contient le nom du produit
-    let productName = document.createElement('h2')
-    itemContentDescription.appendChild(productName)
-    productName.innerText = "productName"
+            // créer un élément div.cart__item__img dans le parent article.cart__item
+            let imgContainer = document.createElement('div')
+            imgContainer.classList.add("cart__item__img")
+            newArticle.appendChild(imgContainer)
 
-    // créer un élément p dans le parent div.cart__item__content__description
-    // contient la couleur du produit
-    let productColor = document.createElement('p')
-    itemContentDescription.appendChild(productColor)
-    productColor.innerText = "productColor"
+            // créer un élément img dans le parent div.cart__item__img
+            // contient l'image du produit
+            let newImg = document.createElement('img')
+            imgContainer.appendChild(newImg)
+            newImg.setAttribute("src", imageUrl)
+            newImg.setAttribute("alt", altTxt)
 
-    // créer un élément p dans le parent div.cart__item__content__description
-    // contient le prix du produit
-    let productPrice = document.createElement('p')
-    itemContentDescription.appendChild(productPrice)
-    productPrice.innerText = 485 + " €"
+            // créer un élément div.cart__item__content dans le parent article.cart__item
+            let itemContent = document.createElement('div')
+            itemContent.classList.add("cart__item__content")
+            newArticle.appendChild(itemContent)
 
-    // créer un élément div.cart__item__content__settings dans le parent div.cart__item__content
-    let contentSettings = document.createElement('div')
-    contentSettings.classList.add("cart__item__content__settings")
-    itemContent.appendChild(contentSettings)
+            // créer un élément div.cart__item__content__description dans le parent div.cart__item__content
+            let itemContentDescription = document.createElement('div')
+            itemContentDescription.classList.add("cart__item__content__description")
+            itemContent.appendChild(itemContentDescription)
 
-    // créer un élément div.cart__item__content__settings__quantity dans le parent div.cart__item__content__settings
-    let contentSettingsQuantity = document.createElement('div')
-    contentSettingsQuantity.classList.add("cart__item__content__settings__quantity")
-    contentSettings.appendChild(contentSettingsQuantity)
+            // créer un élément h2 dans le parent div.cart__item__content__description
+            // contient le nom du produit
+            let productName = document.createElement('h2')
+            itemContentDescription.appendChild(productName)
+            productName.innerText = name
 
-    // créer un élément p dans le parent div.cart__item__content__settings__quantity
-    // contient la quantité du produit
-    let productQuantity = document.createElement('p')
-    contentSettingsQuantity.appendChild(productQuantity)
-    productQuantity.innerText = "Qté: " + 15
+            // créer un élément p dans le parent div.cart__item__content__description
+            // contient la couleur du produit
+            let productColor = document.createElement('p')
+            itemContentDescription.appendChild(productColor)
+            productColor.innerText = color
 
-    // créer un élément input.itemQuantity dans le parent div.cart__item__content__settings__quantity
-    let productQuantityInput = document.createElement('input')
-    productQuantityInput.classList.add("itemQuantity")
-    contentSettingsQuantity.appendChild(productQuantityInput)
-    productQuantityInput.setAttribute("name", "itemQuantity")
-    productQuantityInput.setAttribute("min", 1)
-    productQuantityInput.setAttribute("max", 100)
-    productQuantityInput.setAttribute("value", 42)
+            // créer un élément p dans le parent div.cart__item__content__description
+            // contient le prix du produit
+            let productPrice = document.createElement('p')
+            itemContentDescription.appendChild(productPrice)
+            productPrice.innerText = (price * amount) + " €"
 
-    // créer un élément div.cart__item__content__settings__delete dans le parent div.cart__item__content__settings
-    let contentSettingsDelete = document.createElement('div')
-    contentSettingsDelete.classList.add("cart__item__content__settings__delete")
-    contentSettings.appendChild(contentSettingsDelete)
+            // créer un élément div.cart__item__content__settings dans le parent div.cart__item__content
+            let contentSettings = document.createElement('div')
+            contentSettings.classList.add("cart__item__content__settings")
+            itemContent.appendChild(contentSettings)
 
-    // créer un élément p dans le parent div.cart__item__content__settings__delete
-    let contenteSettingsDeleteItem = document.createElement('p')
-    contenteSettingsDeleteItem.classList.add("deleteItem")
-    contentSettingsDelete.appendChild(contenteSettingsDeleteItem)
-    contenteSettingsDeleteItem.innerText = "Supprimer"
-}
+            // créer un élément div.cart__item__content__settings__quantity dans le parent div.cart__item__content__settings
+            let contentSettingsQuantity = document.createElement('div')
+            contentSettingsQuantity.classList.add("cart__item__content__settings__quantity")
+            contentSettings.appendChild(contentSettingsQuantity)
+
+            // créer un élément p dans le parent div.cart__item__content__settings__quantity
+            // contient la quantité du produit
+            let productQuantity = document.createElement('p')
+            contentSettingsQuantity.appendChild(productQuantity)
+            productQuantity.innerText = "Qté:"
+
+            // créer un élément input.itemQuantity dans le parent div.cart__item__content__settings__quantity
+            let productQuantityInput = document.createElement('input')
+            productQuantityInput.classList.add("itemQuantity")
+            contentSettingsQuantity.appendChild(productQuantityInput)
+            productQuantityInput.setAttribute("name", "itemQuantity")
+            productQuantityInput.setAttribute("min", 1)
+            productQuantityInput.setAttribute("max", 100)
+            productQuantityInput.setAttribute("value", amount)
+
+            // modifie la valeur de "value" ( = la quantité de produits de cette couleur), si on change la quantité manuellement dans l'input
+            productQuantityInput.addEventListener("change", function() {
+                productQuantityInput.setAttribute("value", productQuantityInput.value)
+            })
 
 
-// affiche le nombre total d'articles
-let totalQuantity = document.getElementById("totalQuantity")
-totalQuantity.innerText = "235"
+            // créer un élément div.cart__item__content__settings__delete dans le parent div.cart__item__content__settings
+            let contentSettingsDelete = document.createElement('div')
+            contentSettingsDelete.classList.add("cart__item__content__settings__delete")
+            contentSettings.appendChild(contentSettingsDelete)
 
-// affiche le prix total
-let totalPrice = document.getElementById("totalPrice")
-totalPrice.innerText = "42025"
+            // créer un élément p dans le parent div.cart__item__content__settings__delete
+            let contentSettingsDeleteItem = document.createElement('p')
+            contentSettingsDeleteItem.classList.add("deleteItem")
+            contentSettingsDelete.appendChild(contentSettingsDeleteItem)
+            contentSettingsDeleteItem.innerText = "Supprimer"
+
+            // supprime l'article au clic du bouton "supprimer"
+            contentSettingsDeleteItem.addEventListener('click', function () {
+                newArticle.remove() // supprime du DOM
+
+                // removeLocalStorage() // supprime du localstorage
+                // console.log(cartStorage)
+                // function removeLocalStorage() {
+                //     localStorage.removeItem(id)
+                // }
+            })
+        }
 
 
-// affichage des messages d'erreur dans le formulaire
-// if ()
-let firstNameErrorMsg = document.getElementById("firstNameErrorMsg")
-firstNameErrorMsg.innerText = "test"
+        // affiche le nombre total d'articles
+        let totalQuantity = document.getElementById("totalQuantity")
+        totalQuantity.innerText = 50
 
-let lastNameErrorMsg = document.getElementById("lastNameErrorMsg")
-lastNameErrorMsg.innerText = "test"
+        // affiche le prix total
+        let totalPrice = document.getElementById("totalPrice")
+        totalPrice.innerText = "42025"
 
-let addressErrorMsg = document.getElementById("addressErrorMsg")
-addressErrorMsg.innerText = "test"
 
-let cityErrorMsg = document.getElementById("cityErrorMsg")
-cityErrorMsg.innerText = "test"
+        // affichage des messages d'erreur dans le formulaire
+        // if ()
+        let firstNameErrorMsg = document.getElementById("firstNameErrorMsg")
+        firstNameErrorMsg.innerText = "test"
 
-let emailErrorMsg = document.getElementById("emailErrorMsg")
-emailErrorMsg.innerText = "test"
+        let lastNameErrorMsg = document.getElementById("lastNameErrorMsg")
+        lastNameErrorMsg.innerText = "test"
+
+        let addressErrorMsg = document.getElementById("addressErrorMsg")
+        addressErrorMsg.innerText = "test"
+
+        let cityErrorMsg = document.getElementById("cityErrorMsg")
+        cityErrorMsg.innerText = "test"
+
+        let emailErrorMsg = document.getElementById("emailErrorMsg")
+        emailErrorMsg.innerText = "test"
 
 
 
--- front/js/product.js
+++ front/js/product.js
@@ -6,146 +6,124 @@ fetch("http://localhost:3000/api/products") // appelle l'API
     })
     .then(function (product) {
 
-        const currenttUrl = window.location.href; // récupère l'URL de la page actuelle
+        const currenttUrl = window.location.href // récupère l'URL de la page actuelle
         const url = new URL(currenttUrl);
-        const idUrl = url.searchParams.get("id"); // récupère l'id dans l'URL actuel
+        const idUrl = url.searchParams.get("id") // récupère l'id dans l'URL actuel
 
         const myProduct = product.find((sofa) => { // fonction qui va vérifier dans la liste des produits si on a un id correspondant à l'id contenu dans l'url de la page
             return sofa._id === idUrl
-        });
+        })
 
         if (myProduct) { // donne des instructions lorsque l'url correspond bien à l'id du produit, pour bien faire correspondre les deux
             const { name, description, altTxt, imageUrl, _id: id, price, colors } = myProduct
 
-           
+
 
             // if (idUrl === id) { // donne des instructions lorsque l'url correspond bien à l'id du produit, pour bien faire correspondre les deux
 
-                // créé un nouvel élément <img> dans la classe "item__img"'
-                let imgContainer = document.querySelector(".item__img")
-                let newImg = document.createElement('img')
-                newImg.setAttribute("src", imageUrl)
-                newImg.setAttribute("alt", altTxt)
-                imgContainer.appendChild(newImg)
+            // créé un nouvel élément <img> dans la classe "item__img"'
+            let imgContainer = document.querySelector(".item__img")
+            let newImg = document.createElement('img')
+            newImg.setAttribute("src", imageUrl)
+            newImg.setAttribute("alt", altTxt)
+            imgContainer.appendChild(newImg)
 
-                // ajoute la description du produit correspondant
-                let itemDescription = document.getElementById('description')
-                itemDescription.textContent = description
+            // ajoute la description du produit correspondant
+            let itemDescription = document.getElementById('description')
+            itemDescription.textContent = description
 
-                // ajoute le nom du produit correspondant
-                let itemName = document.getElementById('title')
-                itemName.textContent = name
+            // ajoute le nom du produit correspondant
+            let itemName = document.getElementById('title')
+            itemName.textContent = name
 
-                // ajoute le prix du produit correspondant
-                let itemPrice = document.getElementById('price')
-                itemPrice.textContent = price
+            // ajoute le prix du produit correspondant
+            let itemPrice = document.getElementById('price')
+            itemPrice.textContent = price
 
 
-                insertOptions(colors)
-                function insertOptions(colorList) { // insère les options de couleurs
-                    let itemOptions = document.getElementById('colors')
-                    
-                    for (let i = 0; i < colorList.length; i++) { // pour chaque option de couleur disponible pour le produit, créé une balise HTML "option" avec la valeur de la couleur
-                        let newOption = document.createElement('option')
-                        itemOptions.appendChild(newOption)
-                        newOption.setAttribute("value", colorList[i])
-                        newOption.textContent = colorList[i]
-                    }
+            insertOptions(colors)
+            function insertOptions(colorList) { // insère les options de couleurs
+                let itemOptions = document.getElementById('colors')
+
+                for (let i = 0; i < colorList.length; i++) { // pour chaque option de couleur disponible pour le produit, créé une balise HTML "option" avec la valeur de la couleur
+                    let newOption = document.createElement('option')
+                    itemOptions.appendChild(newOption)
+                    newOption.setAttribute("value", colorList[i])
+                    newOption.textContent = colorList[i]
                 }
+            }
 
-                // }
-
-
-                    ////////////////////////////////////////////////////////////////
-
-            let cartArray = [
-            //     {
-            //     // "id": "",
-            //     // "color": "",
-            //     // "amount": 0
             // }
-        ]
+
+
+            ////////////////////////////////////////////////////////////////
+
+            // panier où sont stockés les articles via localstorage
+            let cartArray = []
 
             let addButton = document.getElementById('addToCart')
             let quantityTotal = 0
 
-
-
+            // mise en place des événements (ajout des produits, quantité, couleurs) lors du clic sur le bouton "ajout au panier"
             addButton.addEventListener('click', function () {
                 let quantity = document.getElementById('quantity')
                 let quantityChosen = quantity.value
                 let colorsList = document.getElementById('colors')
-                let colorChosen = colorsList.options[colorsList.selectedIndex].value // cible la couleur choisie parmi les options disponibles dans la balise HTML "select".colors
+                let colorChosen = colorsList.options[colorsList.selectedIndex].value // cible la couleur choisie parmi les options disponibles dans la balise HTML select.colors
                 // console.log(parseInt(quantityChosen))
 
-                function lookForId() {
-                    cartArray.map(object => {
-                        if (object.id === id) {
-                            return {...object, number: "55"}
-                        }
-                    })
-                }
-
+                // fonction permettant de push l'article vers le panier et le localstorage
                 function insertNewItem() {
-                    cartArray.push({"id": id, "color": colorChosen, "amount": Number(quantityChosen)})
+                    cartArray.push({ "id": id, "color": colorChosen, "amount": Number(quantityChosen) })
                 }
 
                 if (colorChosen == false || quantityChosen == 0) { // si une couleur ou un nombre d'article n'est pas indiqué, renvoie une alerte d'erreur
                     alert("Veuillez choisir une couleur et un nombre d'article à ajouter au panier")
 
-                // } else if (cartArray.includes(myProduct)) { // si le produit est déjà présent dans le panier
-                //     quantityTotal += Number(quantityChosen)
-                //     console.log(parseInt(quantityTotal))
-                //     console.log(quantityTotal)
-                //     console.log("// si le produit est déjà présent dans le panier")
+                    // } else if (cartArray.includes(myProduct)) { // si le produit est déjà présent dans le panier
+                    //     quantityTotal += Number(quantityChosen)
+                    //     console.log(parseInt(quantityTotal))
+                    //     console.log(quantityTotal)
+                    //     console.log("// si le produit est déjà présent dans le panier")
 
-                // product.find((sofa) => { return sofa._id === idUrl })
-                
+                    // product.find((sofa) => { return sofa._id === idUrl })
+
                     // !cartArray.find((product) => { return product.id === myProduct })
-                
-                } else if (!cartArray.includes(myProduct) && !cartArray.includes(colorChosen) && !cartArray.find( product => product.id === id)) { // si le produit et la couleur ne sont pas déjà présents dans le panier
+
+                } else if (!cartArray.includes(myProduct) && !cartArray.includes(colorChosen) && !cartArray.find(product => product.id === id)) { // si le produit et la couleur ne sont pas déjà présents dans le panier
                     quantityTotal += Number(quantityChosen)
+                    insertNewItem() 
+                    console.log("1. ni le produit ni la couleur ne sont pas déjà présents dans le panier")
                     // cartArray.push(id, colorChosen, Number(quantityChosen))
                     // cartArray.push({"id": id, "color": colorChosen, "amount": Number(quantityChosen)})
-                    insertNewItem() // = cartArray.push({"id": id, "color": colorChosen, "amount": Number(quantityChosen)})
+                    // = cartArray.push({"id": id, "color": colorChosen, "amount": Number(quantityChosen)})
                     // newArray(id, colorChosen, quantityTotal)
                     // cartArray.push(colorChosen)
                     // cartArray.productId = id
                     // cartArray.productColor = colorChosen
                     // cartArray.productAmount = quantityChosen
-                    console.log("1. ni le produit ni la couleur ne sont pas déjà présents dans le panier")
 
-                } else if (cartArray.find( product => product.id === id) && !cartArray.find( product => product.color === colorChosen)) { // si le produit est déjà présent mais pas la variation de couleur, on ajoute un nouvel array
+                } else if (cartArray.find(product => product.id === id) && !cartArray.find(product => product.color === colorChosen)) { // si le produit est déjà présent mais pas la variation de couleur, on ajoute un nouvel array
                     // quantityTotal += Number(quantityChosen)
                     insertNewItem() // = cartArray.push({"id": id, "color": colorChosen, "amount": Number(quantityChosen)})
                     console.log("2. produit présent mais nouvelle couleur")
                     // console.log("// si le produit est déjà présent dans le panier")
-    
-                // } else if (cartArray.find( product => product.id === id)){
-                } else if (cartArray.find( product => product.id === id) && cartArray.find( product => product.color === colorChosen)) { // si le produit et la couleur sont déjà présents dans le panier, on ajoute la quantité à la couleur choisie
-                    const newAmount = cartArray.findIndex ((obj => obj.color == colorChosen))
+
+                    // } else if (cartArray.find( product => product.id === id)){
+                } else if (cartArray.find(product => product.id === id) && cartArray.find(product => product.color === colorChosen)) { // si le produit et la couleur sont déjà présents dans le panier, on ajoute la quantité à la couleur choisie
+                    const newAmount = cartArray.findIndex((obj => obj.color == colorChosen))
                     cartArray[newAmount].amount += Number(quantityChosen)
                     console.log("3. produit et couleur déjà présents, actualisation du montant")
                 }
 
-                function newArray(id, colorChosen, quantityTotal) {
-                    cartArray.push(new Array(id, colorChosen, quantityTotal))
-                }
-
-                let cartStorage = JSON.stringify(cartArray) // stocke les données du panier dans le localstorage
+                // stocke les données du panier dans le localstorage en les convertissant en chaîne de caractères
+                let cartStorage = JSON.stringify(cartArray)
                 localStorage.setItem("cart", cartStorage)
-
-
-                // let cartStorageLecture = localStorage.getItem("cart");
-                // let objJson = JSON.parse(cartStorageLecture);
-                // alert(cartStorage)
 
                 console.log(cartArray)
             })
 
-                        // !! recalculer le total du panier dans la page panier, pas à chaque fois dans le local storage
-
         }
 
-        }
+    }
     )
